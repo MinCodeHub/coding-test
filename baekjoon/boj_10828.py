e4ab21f91938ceb43f612d831e8b5f12e334f1e8
me_nyong <gjalsdud1030@naver.com>
2026-03-18 16:45:23 +0900

    refact: boj_10828 - 입력 구문 등 변경

--- baekjoon/boj_10828.py
+++ baekjoon/boj_10828.py
@@ -1,33 +1,29 @@
-n = int(input())
+import sys
+
+# 입력의 개수 N을 받음
+n = int(sys.stdin.readline())
 stack = []
-result = []
 
-for i in range(n):
-    cmd = input().split()
+for _ in range(n):
+    # sys.stdin.readline()으로 입력 받고, 공백을 기준으로 명령어를 구분
+    input_line = sys.stdin.readline().split()
+    command = input_line[0]
 
-    if cmd[0] == "push":
-        stack.append(cmd[1])
-
-    elif cmd[0] == "pop":
-        if len(stack) == 0:
-            result.append("-1")
+    if command == "push":
+        # push 명령어의 경우, 두 번째 요소(정수)를 스택에 추가
+        stack.append(int(input_line[1]))
+    elif command == "pop":
+        if stack:
+            print(stack.pop())
         else:
-            result.append(stack.pop())
-
-    elif cmd[0] == "size":
-        result.append(str(len(stack)))
-
-    elif cmd[0] == "empty":
-        if len(stack) == 0:
-            result.append("1")
+            print(-1)
+    elif command == "size":
+        print(len(stack))
+    elif command == "empty":
+        # 스택이 비어있는지 확인
+        print(0 if stack else 1)
+    elif command == "top":
+        if stack:
+            print(stack[-1])  # 스택의 마지막 요소(가장 위에 있는 정수)를 출력
         else:
-            result.append("0")
-
-    elif cmd[0] == "top":
-        if len(stack) == 0:
-            result.append("-1")
-        else:
-            result.append(stack[-1])
-
-#한 번에 출력
-print("\n".join(result))
+            print(-1)
