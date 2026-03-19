e45336e0d6456b5c76fba63e00dda13722ff920d
me_nyong <gjalsdud1030@naver.com>
2026-03-19 17:33:15 +0900

    solve: boj_10816.py

---
+++ baekjoon/boj_10816.py
@@ -0,0 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+card = input().split()
+
+m = int(input())
+findCard = input().split()
+
+result = {}
+
+for c in card:
+    result[c] = result.get(c,0)+1
+
+for f in findCard:
+    print(result.get(f,0), end=" ")
+
+
+
+# n = (int)(sys.stdin.readline().strip())
+
+# card = sys.stdin.readline().split()
+
+# m = (int)(sys.stdin.readline().strip())
+    
+# findCard = sys.stdin.readline().split()
+
+# result ={}
+
+# #카드 개수 세기
+# for i in range(len(card)):
+#   if(result.get(card[i])):
+#      result[card[i]]+=1
+#   else:
+#     result[card[i]]=1
+ 
+# #결과 출력
+# for j in range(len(findCard)):
+#     if(result.get(findCard[j])):
+#         print(result[findCard[j]])
+#     else:
+#        print(0)
