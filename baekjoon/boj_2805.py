ffe521a300f508f3db4a7ff05d3c51508f180aa9
me_nyong <gjalsdud1030@naver.com>
2026-03-26 09:08:22 +0900

    solve: boj_2805

--- baekjoon/boj_2805.py
+++ baekjoon/boj_2805.py
@@ -6,18 +6,19 @@ n,m = map(int, input().split())
 
 arr = list(map(int, input().split()))
 
-#정렬
-arr.sort()
+#정렬 시간복잡도:O(N log N) 오래 걸림 
+# arr.sort() 
 
 left = 1
-right = arr[-1]
+right = max(arr) #시간복잡도 :O(N) 가벼움, 배열 변경 없음
 answer = 0
-
-while(left <= right):
-    mid = (left+right)//2
-    total = 0
-    for a in arr: #해당포문때문에 시간초과
-        if a > mid:
+ 
+#총 시간 복잡도 O(log N) 이분탐색 + 전체 배열 순회 O(N)
+while left <= right: 
+    mid = (left+right)//2 
+    total = 0 #벌목된 나무 총합
+    for a in arr: 
+        if a >= mid:
             total +=(a - mid)
     
     if total >= m:
